deed584ec306e895e828b913e4be33343df372da
dd8312 <dd8312@att.com>
2016-08-22 20:57:54 -0500

    the rest of the UriImageAdapter changes

--- app/src/main/java/us/dybowski/popularmovies/UriImageAdapter.java
+++ app/src/main/java/us/dybowski/popularmovies/UriImageAdapter.java
@@ -1,9 +1,13 @@
 package us.dybowski.popularmovies;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -12,7 +16,7 @@ import java.util.List;
  */
 public class UriImageAdapter extends ArrayAdapter<String> {
 
-    public static final String BASE_PATH = "http://image.tmdb.org/t/p/w185/";
+    private String mBasePath;
     /**
      * This is our own custom constructor (it doesn't mirror a superclass constructor).
      * The context is used to inflate the layout file, and the List is the data we want
@@ -22,15 +26,24 @@ public class UriImageAdapter extends ArrayAdapter<String> {
      * @param imagePaths A List of strings that when combined with the base path will
      *                   resolve to a uri to an image
      */
-    public UriImageAdapter(Activity context, List<String> imagePaths) {
+    public UriImageAdapter(Activity context, List<String> imagePaths, String basePath) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
 
         super(context, 0, imagePaths);
+        mBasePath = basePath;
+
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        if(convertView == null) {
+            convertView = new ImageView(getContext());
+        }
+        Uri imageUri = Uri.parse(mBasePath).buildUpon().appendPath(getItem(position)).build();
+
+        Picasso.with(getContext()).load(imageUri).into((ImageView)convertView);
+
+        return convertView;
     }
 }
